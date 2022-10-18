14faef4de1e649716afcae2477cf114c0dcde056
sanathdavis <sanathdavis@gmail.com>
2022-10-18 16:26:07 -0400

    Update EmployeeModule.py

--- EmployeeModule.py
+++ EmployeeModule.py
@@ -1,5 +1,22 @@
 #class which contains framework for an employee
 
+ALL_TOGETHER = 0
+ANNUAL = 1
+MONTHLY = 2
+BI_WEEKLY = 3
+
+STUB_TYPES = {
+  ANNUAL : return_bold(return_underline('Annual View')),
+  MONTHLY : return_bold(return_underline('Monthly View')),
+  BI_WEEKLY : return_bold(return_underline('Bi-Weekly View')),
+}
+
+DIVIDE_BY = {    
+  ANNUAL : 1,
+  MONTHLY : 12,
+  BI_WEEKLY : 24,
+}
+
 class Employee:
 
   TAX_RATE = 13.5
