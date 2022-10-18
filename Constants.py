4ee5d7002a2a9bf67b4b28135f79452c16c0a410
sanathdavis <sanathdavis@gmail.com>
2022-10-18 17:32:39 -0400

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -2,7 +2,7 @@
 #defines functions that print error text and success text
 #also resets to normal colors after usage
 
-from .Helpers import *
+#from .Helpers import *
 
 employees = []
 ALL_TOGETHER = 0
@@ -11,9 +11,9 @@ MONTHLY = 2
 BI_WEEKLY = 3
 
 STUB_TYPES = {
-  ANNUAL : return_bold(return_underline('Annual View')),
-  MONTHLY : return_bold(return_underline('Monthly View')),
-  BI_WEEKLY : return_bold(return_underline('Bi-Weekly View')),
+  ANNUAL : 'Annual View',
+  MONTHLY : 'Monthly View',
+  BI_WEEKLY : 'Bi-Weekly View',
 }
 
 DIVIDE_BY = {    
